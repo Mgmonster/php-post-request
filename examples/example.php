a8993f48477094f02644c083b5c72bcccf8829ce
Risan Bagja <risanbagja@yahoo.com>
2013-12-02 00:06:53 +0700

    Call setData method with an invalid argument

--- examples/example.php
+++ examples/example.php
@@ -4,3 +4,6 @@ require_once('../src/post_request.php');
 
 /* Create PostRequest object without any argument */
 $postRequest = new PostRequest();
+
+/* Call setData method with an invalid argument */
+$postRequest->setData('invalid');
