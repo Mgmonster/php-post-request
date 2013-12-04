32198eff51a4cfeb6aed326323562e2212635d6b
Risan Bagja <risanbagja@yahoo.com>
2013-12-04 14:47:50 +0700

    Add an usage example

--- examples/example.php
+++ examples/example.php
@@ -2,5 +2,15 @@
 /* Inclue PostRequest class */
 require_once('../src/post_request.php');
 
-/* Create PostRequest object without any argument */
+/* Create PostRequest instance */
 $postRequest = new PostRequest();
+
+/* Set data to be sent */
+$postRequest->setData(array(
+	'username'	=> 'johndoe',
+	'message'	=> 'hello world!'
+));
+
+/* Send POST request to endpoint */
+$response = $postRequest->send('http://risanbagja1.com');
+var_dump($response);
